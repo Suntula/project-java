527466ff41f51310bc576a84efe4e6f37a884b6b
Suntula <jenishjen2005@gmail.com>
2024-08-16 20:18:35 +0545

    Update MISSystem.java

--- src/MISSystem.java
+++ src/MISSystem.java
@@ -15,7 +15,7 @@ public class MISSystem {
             this.email = email;
         }
 
-        // Getters and Setters
+        
         public String getId() { return id; }
         public void setId(String id) { this.id = id; }
         public String getName() { return name; }
